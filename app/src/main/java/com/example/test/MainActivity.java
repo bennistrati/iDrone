985a52f99dd37d799c275b6c7e844a80a40b813e
Markus <mvogel13@yahoo.de>
2017-11-15 23:01:06 +0100

    Erste Layoutansätze

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/* Usable elements in this layout:
+Start-Button: ID: button_start
+Settings-Button: ID: button_settings
+Gallery-Button: ID: button_gallery
+*/
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
