6472f7084a09d55baa7d0b4bb691df38d8680b68
Benjamin Strathmeier <bennistrati@gmx.net>
2017-11-27 18:22:30 +0100

    basic camera structure

--- app/src/main/java/com/example/test/ThrowActivity.java
+++ app/src/main/java/com/example/test/ThrowActivity.java
@@ -10,8 +10,14 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.provider.MediaStore.Files.FileColumns;
+import android.util.Log;
 
+import java.io.File;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 /* Usable xml-elements in this layout:
 Back-Button: ID: button_backThrow;
 */
@@ -29,18 +35,53 @@ public class ThrowActivity extends AppCompatActivity implements SensorEventListe
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
-    //check about camera
+
+    /* Check if this device has a camera */
     private boolean checkCameraHardware(Context context){
-        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            //returns true if a camera is available
+        if(context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            //this device has a camera
             return true;
         }else{
+            //no camera detected
             return false;
         }
     }
     public static Camera getCameraInstance(){
         Camera c = null;
+        try {
+            c = Camera.open();// attempt to get a camera isntance, camera.open(int) for accesing specific
+        }catch(Exception e){
+            //Camera is not available (in use or doesnt exist)
+        }
+        return c; // returns null if camera is unavailable
     }
+    private PictureCallback mPicture = new PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null){
+                Log.d(TAG, "Error creating media file, check storage permissions: " +
+                        e.getMessage());
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+    };
+
+
+
+
 
 
 
@@ -81,5 +122,6 @@ public class ThrowActivity extends AppCompatActivity implements SensorEventListe
     public void returnToMenuFromThrow(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+
     }
 }
